16a40745b36c29f0d6a12b9040c33793d6e3bcf2
Michal Kašpárek <michal.kasparek@gmail.com>
2024-11-01 08:33:10 +0100

    vypnutí driveru a displeje

--- 001_cd_scrapovani.py
+++ 001_cd_scrapovani.py
@@ -10,10 +10,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pyvirtualdisplay import Display
 
-kam = f"downloads/{datetime.now().strftime('%Y-%m-%d')}"
+kam = f"downloads/{datetime.now().strftime('%Y-%m-%d')}" 
 os.makedirs(kam, exist_ok=True)
 
-def cd(odkud, kam, pocet_dni=0):        
+def cd(odkud, kam, pocet_dni=0):
 
     print(f"{odkud}-{kam} {pocet_dni} d")
 
@@ -45,7 +45,7 @@ def cd(odkud, kam, pocet_dni=0):
 
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, "suggestion-options__link")))
 
-            driver.execute_script("""document.querySelectorAll('.suggestion-options__link').forEach(element => element.style.display = 'none');""")
+            driver.execute_script("""document.querySelectorAll('.suggestion-options__link').forEach(element => element.style.display = 'none');""") ## zakryje element zakrývající posouvadlo dní
 
             kalendar = wait.until(EC.presence_of_element_located((By.ID, "nextday")))
             for i in range(0,pocet_dni):
@@ -62,7 +62,7 @@ def cd(odkud, kam, pocet_dni=0):
         print("Výsledky načteny.")
 
         for i in range(1,7):
-            nacist = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.btn--blue[title='+ 5 dalších']")))
+            nacist = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.btn--blue[title='+ 5 dalších']"))) ## Tohle i u PRG-BRN stačí na následujících 24 hodin.
             sleep(1)
             nacist.click()
 
@@ -78,7 +78,19 @@ def cd(odkud, kam, pocet_dni=0):
         driver.quit()
 
     except Exception as e:
-        print(f"CHYBA: {e}.")
+        print(f"Projáníčka, propáníčka, pro ztracené korále – máme všichni namále: {e}.")
+
+    finally:
+        if driver:
+            try:
+                driver.quit()
+            except Exception as e:
+                print(f"Projáníčka, propáníčka, pro ztracené korále – máme všichni namále: {e}.")
+        if display:
+            try:
+                display.stop()
+            except Exception as e:
+                print(f"Projáníčka, propáníčka, pro ztracené korále – máme všichni namále: {e}.")
 
 trasy = [
     ('Praha','Brno'),
@@ -87,10 +99,12 @@ trasy = [
     ('České Budějovice','Plzeň'),
     ('Znojmo','Jihlava'),
     ('Brno', 'Hamburg'),
-    ('Liberec','Salzburg')
+    ('Liberec','Salzburg'),
+    ('Olomouc','Przemysl')
 ]
 
-dny = [0,1,2,3,4,5,6,7,random.randint(8, 60)]
+dny = [0,1,2,3,4,5,6,7] 
+dny.append(random.randint(8, 60)) ## Stahujeme celý následující týden + náhodné datum v příštích 2 měsících, takže jich za den získáme 24.
 
 for d in dny:
     for x in trasy:
